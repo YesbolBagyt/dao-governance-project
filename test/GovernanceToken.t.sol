d6d9487af30aca5fd6bd6fa1e6d4e83c56e87dc1
YesbolBagyt <yeabolbagyt@gmail.com>
2026-05-05 19:50:10 +0500

    fix governance token tests

--- test/GovernanceToken.t.sol
+++ test/GovernanceToken.t.sol
@@ -2,10 +2,16 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
+
 import "../src/GovernanceToken.sol";
 import "../src/TokenVesting.sol";
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 contract GovernanceTokenTest is Test {
+    using SafeERC20 for IERC20;
+
     GovernanceToken token;
     TokenVesting vesting;
 
@@ -22,19 +28,20 @@ contract GovernanceTokenTest is Test {
             365 days
         );
 
-        token.transfer(address(vesting), 400_000 * 1e18);
+        IERC20(address(token)).safeTransfer(address(vesting), 400_000 * 1e18);
     }
 
-    function testInitialSupply() public {
+    function testInitialSupply() public view {
         assertEq(token.totalSupply(), 1_000_000 * 1e18);
     }
 
-    function testTreasuryBalance() public {
+    function testTreasuryBalance() public view {
         assertEq(token.balanceOf(treasury), 300_000 * 1e18);
     }
 
     function testDelegation() public {
         token.delegate(address(this));
+
         assertEq(token.getVotes(address(this)), token.balanceOf(address(this)));
     }
 
@@ -42,20 +49,23 @@ contract GovernanceTokenTest is Test {
         token.delegate(address(this));
 
         uint256 votes = token.getVotes(address(this));
+
         assertGt(votes, 0);
     }
 
     function testTransferAffectsVotes() public {
         token.delegate(address(this));
 
-        token.transfer(user, 1000);
+        IERC20(address(token)).safeTransfer(user, 1000);
 
         uint256 votes = token.getVotes(address(this));
+
         assertLt(votes, token.totalSupply());
     }
 
-    function testVestingBeforeTime() public {
+    function testVestingBeforeTime() public view {
         uint256 releasable = vesting.releasable();
+
         assertEq(releasable, 0);
     }
 
@@ -63,6 +73,7 @@ contract GovernanceTokenTest is Test {
         vm.warp(block.timestamp + 180 days);
 
         uint256 releasable = vesting.releasable();
+
         assertGt(releasable, 0);
     }
 
@@ -70,6 +81,45 @@ contract GovernanceTokenTest is Test {
         vm.warp(block.timestamp + 365 days);
 
         uint256 releasable = vesting.releasable();
+
         assertEq(releasable, 400_000 * 1e18);
+    }
+
+    function testPermitSignature() public {
+        uint256 privateKey = 0xA11CE;
+        address owner = vm.addr(privateKey);
+        address spender = address(3);
+
+        IERC20(address(token)).safeTransfer(owner, 1000 ether);
+
+        uint256 value = 100 ether;
+        uint256 deadline = block.timestamp + 1 days;
+
+        bytes32 structHash = keccak256(
+            abi.encode(
+                keccak256(
+                    "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
+                ),
+                owner,
+                spender,
+                value,
+                token.nonces(owner),
+                deadline
+            )
+        );
+
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                token.DOMAIN_SEPARATOR(),
+                structHash
+            )
+        );
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, digest);
+
+        token.permit(owner, spender, value, deadline, v, r, s);
+
+        assertEq(token.allowance(owner, spender), value);
     }
 }
